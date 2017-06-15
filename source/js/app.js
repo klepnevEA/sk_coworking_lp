38e8a4f235d98cb4c3b367e58c90a787231341c3
romuald <rarsenev@ustech.ru>
2017-06-15 18:59:52 +0300

    Слайдер внизу

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,44 @@
 'use strict';
 
 $(document).ready(function() {
+	//слайдер верний
 	$(".slider-main__wrap").slick({
-	    dots: false,
-	    arrows: true
+		dots: false,
+		arrows: true
+	});
+	
+
+
+	//слайдер нижний
+	$(".slider-bottom__wrap").slick({
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		dots: true,
+		arrows: true,
+		responsive: [
+			{
+			breakpoint: 1250,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3
+				}
+			},
+			{
+			breakpoint: 1050,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+			breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+			
+		]
 	});
 	
 });
-
-
-
-
