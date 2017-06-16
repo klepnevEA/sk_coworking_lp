2f44d788f204d73d52937367946872cd31274722
Klepnev <klepnev-ea@yandex.ru>
2017-06-16 17:15:02 +0300

    адптация и верхнее меню

--- source/js/app.js
+++ source/js/app.js
@@ -57,4 +57,12 @@ $(document).ready(function() {
 			
 		]
 	});
+
+
+	/*кнопка верхнего меню*/
+
+	$('.dropdown-menu__link').on('touchend mouseup', function() {
+
+		$('.header').toggleClass('open-menu');
+	})
 });
