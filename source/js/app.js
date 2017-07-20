918269c834b3c71b67faebb5e9e68fb311d929d5
Klepnev <klepnev-ea@yandex.ru>
2017-07-20 22:22:46 +0300

    Доработки всякие. Новое модальное окно.

--- source/js/app.js
+++ source/js/app.js
@@ -27,6 +27,18 @@ $(document).ready(function() {
 
 	});
 
+	//табы
+	$(".tabs__link").click(function(e){
+		e.preventDefault();
+		var $that = $(this),
+			$thatVal = $that.data('tab');
+		$('.tabs__link').removeClass('active');
+		$that.addClass('active');
+					
+		$('.your-coworking__item-wrap').removeClass('active');
+		$($thatVal).addClass('active');
+	});
+
 
 
 	//слайдер верний
@@ -37,36 +49,45 @@ $(document).ready(function() {
 	
 
 
-	//слайдер нижний
-	$(".slider-bottom__wrap").slick({
-		slidesToShow: 4,
-		slidesToScroll: 4,
+	// //слайдер нижний
+	// $(".slider-bottom__wrap").slick({
+	// 	slidesToShow: 4,
+	// 	slidesToScroll: 4,
+	// 	dots: true,
+	// 	arrows: true,
+	// 	responsive: [
+	// 		{
+	// 		breakpoint: 1250,
+	// 			settings: {
+	// 				slidesToShow: 3,
+	// 				slidesToScroll: 3
+	// 			}
+	// 		},
+	// 		{
+	// 		breakpoint: 1050,
+	// 			settings: {
+	// 				slidesToShow: 2,
+	// 				slidesToScroll: 2
+	// 			}
+	// 		},
+	// 		{
+	// 		breakpoint: 767,
+	// 			settings: {
+	// 				slidesToShow: 1,
+	// 				slidesToScroll: 1
+	// 			}
+	// 		}
+			
+	// 	]
+	// });
+
+
+	//слайдер отзывво
+	$(".slider-reviews__wrap").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
 		dots: true,
 		arrows: true,
-		responsive: [
-			{
-			breakpoint: 1250,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3
-				}
-			},
-			{
-			breakpoint: 1050,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2
-				}
-			},
-			{
-			breakpoint: 767,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-			
-		]
 	});
 
 
@@ -78,9 +99,15 @@ $(document).ready(function() {
 	});
 
 
-	$('.dropdown-menu__link').on('click', function() {
+	$('.dropdown-menu__link').on('click', function(e) {
+		e.preventDefault();
 		if($( window ).width() <= 1024) {
 			$('.header').toggleClass('open-menu');
 		}
 	})
+
+
+	//$('.selectpicker').selectpicker();
+
+
 });
